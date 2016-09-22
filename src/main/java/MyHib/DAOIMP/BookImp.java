7deccbc5f688df8789eba062f3d0dd5e7741cb24
neftys <Infentri@vips.kiev.ua>
2016-09-22 18:54:36 +0300

    1

--- src/main/java/MyHib/DAOIMP/BookImp.java
+++ src/main/java/MyHib/DAOIMP/BookImp.java
@@ -1,10 +1,23 @@
 package MyHib.DAOIMP;
 
+import MyHib.DAO.Book;
 import MyHib.Util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 /**
  * Created by tu_gevelav on 22.09.2016.
  */
 public class BookImp {
-    HibernateUtil hibernateUtil = new HibernateUtil();
+    private static SessionFactory sessionFactory;
+/*
+    public Integer addBook (Book book){
+        sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction= session.getTransaction();
+        transaction.begin();
+
+    }
+    */
 }
