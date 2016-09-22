30f99b2ade1d2339e26420edfec9561c3008c74e
neftys <Infentri@vips.kiev.ua>
2016-09-22 15:43:04 +0300

    1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,8 @@
  * Created by tu_gevelav on 20.09.2016.
  */
 public class Main {
+    public static void main(String[] args) {
+       Developer developer = new Developer();
+        System.out.println(developer);
+    }
 }
