f8690b64b3b01289dda076d5113c2eabc4adb3bf
Ниф <Infentri@vips.kiev.ua>
2016-09-29 21:31:17 +0300

    1

--- src/main/java/MyHib/WorkSpace/WorkSpace.java
+++ src/main/java/MyHib/WorkSpace/WorkSpace.java
@@ -3,22 +3,14 @@ package MyHib.WorkSpace;
 
 import MyHib.Constructor.Constructor;
 import MyHib.DAO.Author;
-import MyHib.DAO.Book;
-import MyHib.DAO.Genre;
+
 
 /**
  * Created by tu_gevelav on 22.09.2016.
  */
 public class WorkSpace {
     public static void main(String[] args) {
-
-        Book book = new Book("FirstBook");
-        book.getArrayList().add(new Genre("Comedy"));
-        book.getArrayList().add(new Genre("Drama"));
-
         Constructor constructor =new Constructor();
-        constructor.add(book);
-
 
 
 
